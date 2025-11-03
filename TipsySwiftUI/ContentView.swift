f9e3f82985d0ae67c036fe21a6f25069285e621f
OptimusPrime <gulliverraed@gmail.com>
2025-11-03 14:19:18 +0200

    Initial Commit

---
+++ TipsySwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipsySwiftUI
+//
+//  Created by Gulliver Raed on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipsySwiftUI/TipsySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsySwiftUIApp.swift
+//  TipsySwiftUI
+//
+//  Created by Gulliver Raed on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TipsySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
